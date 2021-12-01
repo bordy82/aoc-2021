aa56fd541ee48dd055b54a7f54c609f7ac3d6544
Kevin Borduas <kborduas@gmail.com>
2021-12-01 12:56:15 -0500

    setup for day 2

---
+++ AoC Day 02/Program.cs
@@ -0,0 +1,18 @@
+﻿using Utilities;
+
+SolvePuzzleOne();
+SolvePuzzleTwo();
+
+void SolvePuzzleOne()
+{
+    var data = DataLoader.GetIntDataFromFile(true);
+
+    Console.WriteLine($"Réponse 1 : ");
+}
+
+void SolvePuzzleTwo()
+{
+    var data = DataLoader.GetIntDataFromFile(true);
+
+    Console.WriteLine($"Réponse 2 : ");
+}
