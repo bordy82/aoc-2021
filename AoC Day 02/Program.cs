9336b13622b2d12ca9d0b799f3918859045d568c
Kevin Borduas <kborduas@gmail.com>
2021-12-02 00:10:19 -0500

    day 2 solution

--- AoC Day 02/Program.cs
+++ AoC Day 02/Program.cs
@@ -5,24 +5,59 @@ SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetIntDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    for (var i = 1; i < data.Length; i++)
+    var currentPosition = 0;
+    var currentDepth = 0;
+
+    for (var i = 0; i < data.Length; i++)
     {
-        
+        var command = data[i].Split(' ');
+
+        switch (command[0])
+        {
+            case "forward":
+                currentPosition += Int32.Parse(command[1]);
+                break;
+            case "down":
+                currentDepth += Int32.Parse(command[1]);
+                break;
+            case "up":
+                currentDepth -= Int32.Parse(command[1]);
+                break;
+
+        }
     }
 
-    Console.WriteLine($"Réponse 1 : ");
+    Console.WriteLine($"Réponse 1 : {currentPosition*currentDepth}");
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetIntDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    for (var i = 1; i < data.Length; i++)
+    var currentPosition = 0;
+    var currentDepth = 0;
+    var currentAim = 0;
+
+    for (var i = 0; i < data.Length; i++)
     {
+        var command = data[i].Split(' ');
 
+        switch (command[0])
+        {
+            case "forward":
+                currentPosition += Int32.Parse(command[1]);
+                currentDepth += (currentAim * Int32.Parse(command[1]));
+                break;
+            case "down":
+                currentAim += Int32.Parse(command[1]);
+                break;
+            case "up":
+                currentAim -= Int32.Parse(command[1]);
+                break;
+        }
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    Console.WriteLine($"Réponse 2 : {currentPosition * currentDepth}");
 }
