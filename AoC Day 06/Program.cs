82c842f9cfb82f3cb554215c1159cb6f12bfa44b
Kevin Borduas <kborduas@gmail.com>
2021-12-06 12:21:05 -0500

    solution day 6

--- AoC Day 06/Program.cs
+++ AoC Day 06/Program.cs
@@ -5,24 +5,48 @@ SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
+    var fishes = data[0].Split(',').Select(x => Int32.Parse(x)).ToList<int>();
 
-    for (var i = 0; i < data.Length; i++)
+    for(var i = 0; i < 80; i++)
     {
+        var fishesToAdd = fishes.Count(x => x == 0);
+        for (var j = 0; j < fishesToAdd; j++)
+            fishes.Add(9);
 
+        for (var j = 0; j < fishes.Count; j++)
+            if (fishes[j] == 0)
+                fishes[j] = 7;
+
+        for (var j = 0; j < fishes.Count; j++)
+            fishes[j]--;
     }
 
-    Console.WriteLine($"Réponse 1 : ");
+    Console.WriteLine($"Réponse 1 : {fishes.Count()}");
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    for (var i = 0; i < data.Length; i++)
+    var fishTable = new Dictionary<int, long>();
+    for(var i = 0; i <= 8; i++)
+        fishTable[i] = 0;
+
+    foreach (var fish in data[0].Split(',').Select(x => Int32.Parse(x)))
+        fishTable[fish]++;
+
+    for (var i = 0; i < 256; i++)
     {
+        var temp0Table = fishTable[0];
 
+        for (var j = 0; j <= 7; j++)
+            fishTable[j] = fishTable[j+1];
+        
+        // 0 deviennent 6 et spawn des 8;
+        fishTable[8] = temp0Table;
+        fishTable[6] += temp0Table;
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    Console.WriteLine($"Réponse 2 : {fishTable.Sum(x => x.Value)}");
 }
