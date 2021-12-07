a7426cf96ebe9293dbec2ad6b8cbed459aef0771
Kevin Borduas <kborduas@gmail.com>
2021-12-07 00:10:10 -0500

    solution day 7

--- AoC Day 07/Program.cs
+++ AoC Day 07/Program.cs
@@ -5,24 +5,48 @@ SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
+    var crabs = data[0].Split(',').Select(x => Int32.Parse(x)).ToList<int>();
 
-    for (var i = 0; i < data.Length; i++)
+    var minValue = crabs.Min();
+    var maxValue = crabs.Max();
+
+    var lowestCost = int.MaxValue;
+    for(var i = minValue; i <= maxValue; i++)
     {
+        var cost = 0;
+        foreach (var c in crabs)
+            cost += Math.Abs(i - c);
 
+        if (cost < lowestCost)
+            lowestCost = cost;
     }
 
-    Console.WriteLine($"Réponse 1 : ");
+    Console.WriteLine($"Réponse 1 : {lowestCost}");
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
+    var crabs = data[0].Split(',').Select(x => Int32.Parse(x)).ToList<int>();
 
-    for (var i = 0; i < data.Length; i++)
+    var minValue = crabs.Min();
+    var maxValue = crabs.Max();
+
+    var lowestCost = int.MaxValue;
+    for (var i = minValue; i <= maxValue; i++)
     {
+        var cost = 0;
+        foreach (var c in crabs)
+        {
+            var steps = Math.Abs(i - c);
+            for (var j = 1; j <= steps; j++)
+                cost += j;
+        }
 
+        if (cost < lowestCost)
+            lowestCost = cost;
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    Console.WriteLine($"Réponse 2 : {lowestCost}");
 }
