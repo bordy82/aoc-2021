7c4ad155ec69bee7f812522215ba647d26f1e564
Kevin Borduas <kborduas@gmail.com>
2021-12-13 22:01:38 -0500

    solution for day 12

---
+++ AoC Day 12/Cave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day_12
+{
+    internal class Cave
+    {
+        public Cave(string name)
+        {
+            this.Name = name;
+            this.ConnectedCaves = new List<Cave>();
+
+            if (name == name.ToLower())
+                this.IsSmallCave = true;
+        }
+
+        public bool IsSmallCave { get; private set; }
+
+        public string Name { get; set; }
+
+        public List<Cave> ConnectedCaves { get; private set; }
+    }
+}
--- AoC Day 12/Program.cs
+++ AoC Day 12/Program.cs
@@ -1,28 +1,120 @@
 ﻿using Utilities;
+using AoC_Day_12;
 
 SolvePuzzleOne();
 SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    for (var i = 0; i < data.Length; i++)
+    var caves = LoadCaves(data);
+
+    var startingCave = caves.First(x => x.Name == "start");
+
+    var paths = new List<string>();
+    foreach (var cave in startingCave.ConnectedCaves)
+        PathFindFor1(cave, startingCave.Name, paths);
+
+    Console.WriteLine($"Réponse 1 : {paths.Count}");
+}
+
+void PathFindFor1(Cave cave, string path, List<string> paths)
+{
+    var visitedCaves = path.Split(',');
+    var newPath = path + "," + cave.Name;
+
+    if (cave.Name != "end")
     {
-
+        foreach (var innerCave in cave.ConnectedCaves)
+        {
+            if (!innerCave.IsSmallCave || !visitedCaves.Any(x => x == innerCave.Name))
+            {
+                PathFindFor1(innerCave, newPath, paths);
+            }
+        }
     }
-
-    Console.WriteLine($"Réponse 1 : ");
+    else
+    {
+        paths.Add(newPath);
+    }
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
+    var caves = LoadCaves(data);
+
+    var startingCave = caves.First(x => x.Name == "start");
+
+    var paths = new List<string>();
+    foreach (var cave in startingCave.ConnectedCaves)
+        PathFindFor2(cave, startingCave.Name, paths);
+
+    Console.WriteLine($"Réponse 2 : {paths.Count}");
+}
+
+void PathFindFor2(Cave cave, string path, List<string> paths)
+{
+    var visitedCaves = path.Split(',');
+    var newPath = path + "," + cave.Name;
+
+    if (cave.Name != "end")
+    {
+        foreach (var innerCave in cave.ConnectedCaves.Where(x => x.Name != "start"))
+        {
+            // HERE
+            if (!innerCave.IsSmallCave || !visitedCaves.Any(x => x == innerCave.Name) || CanVisitInnerCaveTwice(newPath))
+            {
+                PathFindFor2(innerCave, newPath, paths);
+            }
+        }
+    }
+    else
+    {
+        paths.Add(newPath);
+    }
+}
+
+bool CanVisitInnerCaveTwice(string path)
+{
+    var visitedCaves = path.Split(',');
+    var visitedSmallCaveTwice = false;
+
+    foreach (var visitedCave in visitedCaves.Where(x => x == x.ToLower()))
+        if (visitedCaves.Count(x => x == visitedCave) >= 2)
+            visitedSmallCaveTwice = true;
+
+    if (visitedSmallCaveTwice)
+        return false;
+    else
+        return true;
+}
+
+List<Cave> LoadCaves(string[] data)
+{
+    var caves = new List<Cave>();
     for (var i = 0; i < data.Length; i++)
     {
+        var caveAName = data[i].Split('-')[0];
+        var caveBName = data[i].Split('-')[1];
 
+        var caveA = new Cave(caveAName);
+        if (!caves.Any(x => x.Name == caveAName))
+            caves.Add(caveA);
+        else
+            caveA = caves.First(x => x.Name == caveAName);
+
+        var caveB = new Cave(caveBName);
+        if (!caves.Any(x => x.Name == caveBName))
+            caves.Add(caveB);
+        else
+            caveB = caves.First(x => x.Name == caveBName);
+
+        caveA.ConnectedCaves.Add(caveB);
+        caveB.ConnectedCaves.Add(caveA);
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    return caves;
 }
